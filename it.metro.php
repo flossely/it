3def6c6461743ea79ce7fcf202ecee24e0965532
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-14 04:34:52 +0000

    Improved functionality

--- it.metro.php
+++ it.metro.php
@@ -14,6 +14,21 @@ $metro['it'] =
         "weight" => "Peso",
         "size" => "Taglia",
     ],
+    "zodiac" =>
+    [
+        1 => "Aquarius",
+        2 => "Pisces",
+        3 => "Aries",
+        4 => "Taurus",
+        5 => "Gemini",
+        6 => "Cancer",
+        7 => "Leo",
+        8 => "Virgo",
+        9 => "Libra",
+        10 => "Scorpio",
+        11 => "Sagittarius",
+        12 => "Capricorn",
+    ],
     "unit" =>
     [
         "ratio" => "",
